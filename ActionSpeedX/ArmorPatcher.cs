53677211794acc80ebc5145913672d317ff1fe3a
holaholacocacola <beefbus5@gmail.com>
2021-02-07 15:39:30 -0800

    ASX-dev pr feedback. Updated materials.json. Swapped key->materialtype, val-tier(int). Refactored npc, armor patchers to be their own classes. Fixed material loop to loop once not n^2

---
+++ ActionSpeedX/ArmorPatcher.cs
@@ -0,0 +1,205 @@
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.FormKeys.SkyrimSE;
+using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.Synthesis;
+using Newtonsoft.Json.Linq;
+using Noggog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ActionSpeedX
+{
+
+
+    public class ArmorPatcher
+    {
+
+        const string ATTACK_SPEED   = "AttackSpeed";
+        const string MOVEMENT_SPEED = "MoveSpeed";
+        const string POWER_ATTACK   = "PowerAttacks";
+        const string SPELL_COST     = "SpellCosts";
+        const string MAGICKA        = "MagickaRegen";
+        const string STAMINA        = "StaminaRegen";
+        const string RANGED_ATTACK  = "RangedAttack";
+
+        const string LIGHT = "Light";
+        //const string MEDIUM         = "Medium";
+        const string HEAVY = "Heavy";
+
+        const string GAUNTLETS = "Gauntlets";
+        const string HELMET    = "Helmet";
+        const string CUIRASS   = "Cuirass";
+        const string BOOTS     = "Boots";
+        const string SHIELD    = "Shield";
+
+        const string MATERIALS_FILE = "armor_materials.json";
+        const string DESCRIPTIONS_FILE = "armor_descriptions.json";
+
+
+        private IPatcherState<ISkyrimMod, ISkyrimModGetter> state;
+        private ActionSpeedX.Settings settings; // in memory rep of settings.json
+        private Dictionary<string, bool> descriptionSettings; 
+        private JObject armorDescriptions; // in memory rep of armor_descriptions.json
+        private Dictionary<string, int> materialRanks; // In memory rep of armor_materials.json
+
+        public ArmorPatcher(IPatcherState<ISkyrimMod, ISkyrimModGetter> state, ActionSpeedX.Settings settings)
+        {
+            this.state = state;
+            this.settings = settings;
+            // This is looped through when adding descriptions. See PatchArmorDescription for usage.
+            this.descriptionSettings = new Dictionary<string, bool> {
+                {ATTACK_SPEED, this.settings.AttackSpeed },
+                {MOVEMENT_SPEED, this.settings.MoveSpeed},
+                {MAGICKA, this.settings.MagickaRegen},
+                {POWER_ATTACK, this.settings.PowerAttacks },
+                {RANGED_ATTACK, this.settings.RangedAttack },
+                {SPELL_COST, this.settings.SpellCosts },
+                {STAMINA, this.settings.StaminaRegen }
+            };
+
+            this.materialRanks = loadMaterials();
+            this.armorDescriptions = loadDescriptions();
+        }
+
+        public void PatchArmors()
+        {
+           
+            foreach (var armor in this.state.LoadOrder.PriorityOrder.WinningOverrides<IArmorGetter>())
+            {
+                if (armor.EditorID == null || armor.Keywords == null || armor.BodyTemplate == null || armor.BodyTemplate.ArmorType == ArmorType.Clothing) continue;
+
+                string armorType;
+                Dictionary<string, List<FormKey>> armorKeysMap;
+                if (armor.BodyTemplate.ArmorType == ArmorType.LightArmor)
+                {
+                    armorKeysMap = ActionSpeedX.FormKeys.Keywords.LightArmorKeywordCollection;
+                    armorType = LIGHT;
+                }
+                else
+                {
+                    armorKeysMap = ActionSpeedX.FormKeys.Keywords.HeavyArmorKeywordCollection;
+                    armorType = HEAVY;
+                }
+
+                int tier = -1;
+                /* We want to grab the highest material tier as some items can have multiple material keywrods
+                   Loop over the armorRankings(see armor_materials.json) and check the current armors keywords for a match.
+                   Item.key = int/str, item.val=List(armor material strings)
+                */
+
+                foreach(var keyword in armor.Keywords)
+                {
+                    if (keyword.TryResolve(state.LinkCache, out var kw))
+                    {
+                        if (kw.EditorID!= null && this.materialRanks.ContainsKey(kw.EditorID))
+                        {
+                            int rank = this.materialRanks[kw.EditorID];
+                            if (rank > tier) tier = rank;
+                        }
+                    }
+                }
+               
+                if (tier < 0)
+                {
+                    Console.WriteLine($"No recognized material type for {armor.EditorID}. Defaulting to tier 2 ");
+                    tier = 1;
+                }
+                else
+                {
+                    tier--; //correct array access
+                }
+                string slot;
+
+                if (armor.Keywords.Contains(Skyrim.Keyword.ArmorBoots)) slot = BOOTS;
+                else if (armor.Keywords.Contains(Skyrim.Keyword.ArmorCuirass)) slot = CUIRASS;
+                else if (armor.Keywords.Contains(Skyrim.Keyword.ArmorGauntlets)) slot = GAUNTLETS;
+                else if (armor.Keywords.Contains(Skyrim.Keyword.ArmorHelmet)) slot = HELMET;
+                else if (armor.Keywords.Contains(Skyrim.Keyword.ArmorShield)) slot = SHIELD;
+                else
+                {
+                    Console.WriteLine("No matching equip slot for " + armor.EditorID);
+                    continue;
+                }
+                var nw = state.PatchMod.Armors.GetOrAddAsOverride(armor);
+                nw.Keywords?.Add(armorKeysMap[slot][tier]);
+                if (this.settings.Descriptions) PatchArmorDescription(nw, armorType, slot, tier);
+            }
+        }
+        
+        private void PatchArmorDescription(Armor armor, string armorType, string armorSlot, int armorTier)
+        {
+
+            // Shields and Boots share the same description magnitudes. Shield is not defined in armor_descriptions.json
+            if (armorSlot == SHIELD) armorSlot = BOOTS;
+
+            //Helmets and Gauntlets share the same description magnitude. Gauntlet is not defined in armor_descriptions.json
+            if (armorSlot == HELMET) armorSlot = GAUNTLETS;
+
+            // add descriptions really ugly dont look
+            string newDescription = "";
+            foreach (var item in this.descriptionSettings)
+            {
+                if (item.Value)
+                {
+                    try
+                    {
+                        string description = "";
+                        string magnitude = "";
+                        JToken? dex = this.armorDescriptions.SelectToken($"Descriptions.{item.Key}");
+                        if (dex != null)
+                        {
+                            description = dex.ToString();
+                        }
+                        JToken? mag = this.armorDescriptions.SelectToken($"Magnitudes.{armorSlot}.{item.Key}.{armorType}[{armorTier}]");
+                        if (mag != null)
+                        {
+                            magnitude = mag.ToString();
+                        }
+
+                        newDescription += description.Replace("{", magnitude);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+            }
+            if (armor.Description?.String.IsNullOrEmpty() ?? true)
+            {
+                armor.Description = newDescription;
+            }
+            else
+            {
+                if (armor.Description?.String?.EndsWith(".") ?? false)
+                {
+                    armor.Description = armor.Description?.String + " " + newDescription;
+                }
+                else
+                {
+                    armor.Description = armor.Description?.String + ". " + newDescription;
+                }
+            }
+        }
+
+
+        private Dictionary<string, int> loadMaterials()
+        {
+            string file = Path.Combine(this.state.ExtraSettingsDataPath, MATERIALS_FILE); // already validated in callee
+            var armorMaterials = JObject.Parse(File.ReadAllText(file));
+            Dictionary<string, int>? materials = armorMaterials.ToObject<Dictionary<string, int>>();
+            if (materials == null)
+            {
+                throw new Exception("Could not parse armor materials file");
+            }
+            return materials;
+        }
+
+        private JObject loadDescriptions()
+        {
+            string file = Path.Combine(this.state.ExtraSettingsDataPath, DESCRIPTIONS_FILE); // already validated in callee
+            return JObject.Parse(File.ReadAllText(file));
+        }
+
+    }
+}
--- ActionSpeedX/Program.cs
+++ ActionSpeedX/Program.cs
@@ -15,24 +15,6 @@ namespace ActionSpeedX
 {
     public class Program
     {
-        const string ATTACK_SPEED   = "AttackSpeed";
-        const string MOVEMENT_SPEED = "MoveSpeed";
-        const string POWER_ATTACK   = "PowerAttacks";
-        const string SPELL_COST     = "SpellCosts";
-        const string MAGICKA        = "MagickaRegen";
-        const string STAMINA        = "StaminaRegen";
-        const string RANGED_ATTACK  = "RangedAttack"; 
-        
-        const string LIGHT          = "Light";
-        //const string MEDIUM         = "Medium";
-        const string HEAVY          = "Heavy";
-
-        const string GAUNTLETS      = "Gauntlets";
-        const string HELMET         = "Helmet";
-        const string CUIRASS        = "Cuirass";
-        const string BOOTS          = "Boots";
-        const string SHIELD         = "Shield";
-
 
         static ModKey Adamant = ModKey.FromNameAndExtension("Adamant.esp");
         static ModKey Vokrii = ModKey.FromNameAndExtension("Vokrii - Minimalistic Perks of Skyrim.esp");
@@ -42,6 +24,7 @@ namespace ActionSpeedX
         {
             return await SynthesisPipeline.Instance
                 .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
+                .AddRunnabilityCheck(RunnabilityCheck)
                 .Run(args, new RunPreferences()
                 {
                     ActionsForEmptyArgs = new RunDefaultPatcher()
@@ -93,8 +76,6 @@ namespace ActionSpeedX
             if (!requiredFiles.SequenceEqual(foundFiles))
                 throw new Exception("Missing one of the following json files in the Data folder: armor_descriptions, armor_materials, races");
 
-            var armorDescriptions = JObject.Parse(File.ReadAllText(requiredFiles[0]));
-            var armorMaterials = JObject.Parse(File.ReadAllText(requiredFiles[1]));
             var availableRaces = JObject.Parse(File.ReadAllText(requiredFiles[2]));
             ActionSpeedX.Settings patchSettings = JsonConvert.DeserializeObject<ActionSpeedX.Settings>(File.ReadAllText(requiredFiles[3]));// This could fail if user messes with settings but w/e
 
@@ -112,24 +93,6 @@ namespace ActionSpeedX
                 if (creatures != null) races.AddRange(creatures.ToList());
             }
 
-            //see PatchArmor for usage.
-            Dictionary<String, List<JToken>> materialRanks = new Dictionary<string, List<JToken>>();
-            foreach (var item in armorMaterials)
-            {
-                materialRanks[item.Key] = item.Value.ToList();
-            }
-
-            // This is looped through when adding descriptions. See PatchArmor for usage.
-            Dictionary<String, bool> descriptionSettings = new Dictionary<string, bool> {
-                {ATTACK_SPEED, patchSettings.AttackSpeed },
-                {MOVEMENT_SPEED, patchSettings.MoveSpeed},
-                {MAGICKA, patchSettings.MagickaRegen},
-                {POWER_ATTACK, patchSettings.PowerAttacks },
-                {RANGED_ATTACK, patchSettings.RangedAttack },
-                {SPELL_COST, patchSettings.SpellCosts },
-                {STAMINA, patchSettings.StaminaRegen }
-            };
-
             // see PatchNpc for usage
             List<FormKey> perkstoAdd = LoadPerks(patchSettings);
             if (perkstoAdd.Count() == 0)
@@ -137,24 +100,16 @@ namespace ActionSpeedX
                 throw new Exception("No settings enabled");
             }
 
-             // Armor Patcher. Adds keywords
-            foreach (var armor in state.LoadOrder.PriorityOrder.WinningOverrides<IArmorGetter>())
-            {
-                if (armor.EditorID == null || armor.Keywords == null) continue;
-                if (PatchArmor(state, armor, materialRanks, armorDescriptions, patchSettings.Descriptions, descriptionSettings)) System.Console.WriteLine($"Successfully patched  + {armor.EditorID}");
-            }
+            // Armor Patcher. Adds keywords
+            Console.WriteLine("Patching armors");
+            ActionSpeedX.ArmorPatcher armorPatcher = new ArmorPatcher(state, patchSettings);
+            armorPatcher.PatchArmors();
 
             // Npc patcher. AddsPerks
+            Console.WriteLine("Patching npcs");
+            ActionSpeedX.NpcPatcher npcPatcher = new NpcPatcher(state, patchSettings);
+            npcPatcher.PatchNpcs();
 
-            foreach (var npc in state.LoadOrder.PriorityOrder.WinningOverrides<INpcGetter>())
-            {
-                if (npc.Configuration.TemplateFlags.HasFlag(NpcConfiguration.TemplateFlag.SpellList)) continue; // Perks are inherited from a template
-                if (npc.EditorID == null) continue;
-                if (npc.Keywords != null && npc.Keywords.Contains(Skyrim.Keyword.ActorTypeGhost)) continue; // Ghost shouldnt be affected by armor
-                if (!npc.Race.TryResolve(state.LinkCache, out var race) || race.EditorID == null || !races.Contains(race.EditorID)) continue; 
-                
-                if (PatchNpc(state, npc, perkstoAdd, patchSettings.Racials, patchSettings.Factions)) System.Console.WriteLine($"Successfully patched  + {npc.EditorID}");
-            }
 
             // PerkMod Patcher. Modifies magnitudes.
             if (!patchSettings.BalancePerkMods) return;
@@ -184,178 +139,8 @@ namespace ActionSpeedX
             }
         }
 
-        private static bool PatchArmor(IPatcherState<ISkyrimMod, ISkyrimModGetter> state, IArmorGetter armor, Dictionary<String, List<JToken>> materialRanks , JObject armorDescriptions, bool patchDescriptions, Dictionary<String, bool> descriptionSettings)
-        {
-
-            string armorType; 
-            Dictionary<string, List<FormKey>> armorKeysMap ;
-            if (armor.BodyTemplate == null)
-            {
-                return false;
-            }
-            if (armor.BodyTemplate.ArmorType == ArmorType.LightArmor)
-            {
-                armorKeysMap = ActionSpeedX.FormKeys.Keywords.LightArmorKeywordCollection;
-                armorType = LIGHT;
-            }
-            else if (armor.BodyTemplate.ArmorType ==  ArmorType.HeavyArmor)
-            {
-                armorKeysMap = ActionSpeedX.FormKeys.Keywords.HeavyArmorKeywordCollection;
-                armorType = HEAVY;
-            }
-            else
-            {
-                return false;
-            }
-
-            int tier = -1;
-            if (armor.Keywords == null) return false;// This is checked before this func is called but compiler complains if we dont check it again :|
-
-            /* We want to grab the highest material tier as some items can have multiple material keywrods
-               Loop over the armorRankings(see armor_materials.json) and check the current armors keywords for a match.
-               Item.key = int/str, item.val=List(armor material strings)
-            */
-            foreach (var item in materialRanks)  //Garbage running time
-            {
-                foreach (var keyword in armor.Keywords)
-                {
-                    if (keyword.TryResolve(state.LinkCache, out var kw))
-                    {
-                        //Console.WriteLine(item.Key);
-                        if (kw.EditorID != null && item.Value.Contains(kw.EditorID))
-                        {
-                            tier = Int32.Parse(item.Key) - 1; // This is always guaranteed to be a string. Dont flame me bic boi.
-                            break;
-                        }
-                    }      
-                }
-            }
-
-            if (tier < 0)
-            {
-                Console.WriteLine($"No recognized material type for {armor.EditorID}. Defaulting to tier 2 " );
-                tier = 1;
-            }
-
-            string slot;
-
-            if (armor.Keywords.Contains(Skyrim.Keyword.ArmorBoots)) slot = BOOTS;
-            else if (armor.Keywords.Contains(Skyrim.Keyword.ArmorCuirass)) slot = CUIRASS;
-            else if (armor.Keywords.Contains(Skyrim.Keyword.ArmorGauntlets)) slot = GAUNTLETS;
-            else if (armor.Keywords.Contains(Skyrim.Keyword.ArmorHelmet)) slot = HELMET;
-            else if (armor.Keywords.Contains(Skyrim.Keyword.ArmorShield)) slot = SHIELD;
-            else
-            {
-               Console.WriteLine("No matching equip slot for " + armor.EditorID);
-                return false;
-            }
-            var nw = state.PatchMod.Armors.GetOrAddAsOverride(armor);
-            nw.Keywords?.Add(armorKeysMap[slot][tier]);
-
-            if (!patchDescriptions) return true;
-
-            // Shields and Boots share the same description magnitudes. Shield is not defined in armor_descriptions.json
-            if (slot == SHIELD) slot = BOOTS;
-
-            //Helmets and Gauntlets share the same description magnitude. Gauntlet is not defined in armor_descriptions.json
-            if (slot == HELMET) slot = GAUNTLETS;
-           
-            // add descriptions really ugly dont look
-            string newDescription = "";
-            foreach(var item in descriptionSettings)
-            {
-                if (item.Value)
-                {
-                    try
-                    {
-                        string description = "";
-                        string magnitude = "";
-                        JToken? dex = armorDescriptions.SelectToken($"Descriptions.{item.Key}");
-                        if (dex != null)
-                        {
-                            description = dex.ToString();
-                        }
-                        JToken? mag = armorDescriptions.SelectToken($"Magnitudes.{slot}.{item.Key}.{armorType}[{tier}]");
-                        if (mag != null)
-                        {
-                            magnitude = mag.ToString();
-                        }
-                    
-                        newDescription += description.Replace("{", magnitude);
-                    } catch (Exception e)
-                    {
-                        Console.WriteLine(e);
-                    }
-                }
-            }
-            if (nw.Description?.String.IsNullOrEmpty() ?? true)
-            {
-                nw.Description = newDescription;
-            }
-            else
-            {
-                if (nw.Description?.String?.EndsWith(".") ?? false)
-                {
-                    nw.Description = nw.Description?.String + " " + newDescription;
-                }
-                else
-                {
-                    nw.Description = nw.Description?.String + ". " + newDescription;
-                }
-            }
-
-            return true;
-        }
-
-        private static bool PatchNpc(IPatcherState<ISkyrimMod, ISkyrimModGetter> state, INpcGetter npc, List<FormKey> perksToAdd, bool addRacial, bool addFactions)
-        {
-
-            var npcCopy = state.PatchMod.Npcs.GetOrAddAsOverride(npc);
-            if (npcCopy.Perks == null) npcCopy.Perks = new ExtendedList<PerkPlacement>();
-            foreach (var perk in perksToAdd)
-            {
-                PerkPlacement p = new PerkPlacement { Rank = 1, Perk = perk};
-                npcCopy.Perks.Add(p);
-            }
-
-            if (npc.EditorID == "Player") // will c# cache this????
-            {
-               if(addRacial)
-                {
-                    // a quest runs after racemenu that will sift and apply the correct racial perk
-                    PerkPlacement p = new PerkPlacement { Rank = 1, Perk = ActionSpeedX.FormKeys.Perks.ASX_DummyPerk};
-                    npcCopy.Perks.Add(p);
-                }
-                return true;
-            }
-
-            if (addRacial)
-            {
-                if (npc.Race.TryResolve(state.LinkCache, out var race) && race.EditorID != null && ActionSpeedX.FormKeys.Perks.RacialPerks.ContainsKey(race.EditorID))
-                {
-                    PerkPlacement p = new PerkPlacement { Rank = 1, Perk = ActionSpeedX.FormKeys.Perks.RacialPerks[race.EditorID]};
-                    npcCopy.Perks.Add(p);
-                }
-            }
-
-            if (addFactions)
-            {
-                if (npc.Factions != null)
-                {
-                    foreach (var faction in npc.Factions)
-                    {
-                        if(faction.Faction.TryResolve(state.LinkCache, out var wtf) && wtf.EditorID != null && ActionSpeedX.FormKeys.Perks.FactionPerks.ContainsKey(wtf.EditorID))
-                        {
-                            PerkPlacement p = new PerkPlacement { Rank = 1, Perk = ActionSpeedX.FormKeys.Perks.FactionPerks[wtf.EditorID] };
-                            npcCopy.Perks.Add(p);
-                        }
-                    }
-                }
-            }
-            return true;
-        }
-
-        private static bool RebalancePerks(IPatcherState<ISkyrimMod, ISkyrimModGetter> state, ActionSpeedX.Settings patchSettings, List<FormKeys.SpellModifier> staminaSpells, List<FormKeys.SpellModifier> speedSpells)
+       
+        private static bool RebalancePerks(IPatcherState<ISkyrimMod, ISkyrimModGetter> state, ActionSpeedX.Settings patchSettings, List<FormKeys.SpellEffectModifier> staminaSpells, List<FormKeys.SpellEffectModifier> speedSpells)
         {
 
             bool hasError = false;
@@ -378,7 +163,7 @@ namespace ActionSpeedX
             return !hasError;
         }
 
-        private static bool ModifySpell(IPatcherState<ISkyrimMod, ISkyrimModGetter> state, ActionSpeedX.FormKeys.SpellModifier spellToModify)
+        private static bool ModifySpell(IPatcherState<ISkyrimMod, ISkyrimModGetter> state, ActionSpeedX.FormKeys.SpellEffectModifier spellToModify)
         {
             if (!state.LinkCache.TryResolve<ISpellGetter>(spellToModify.formKey, out var spell))
             {
